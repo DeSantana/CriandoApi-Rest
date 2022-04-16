f12bcdddb9ace7a09a3d5935b84ad064bddb1923
João Victor <85123879+DeSantana@users.noreply.github.com>
2022-04-16 08:35:52 -0300

    API REST

---
+++ tutorial-rest-api/src/main/java/expertostech/tutorial/rest/api/repository/UsuarioRepository.java
@@ -0,0 +1,10 @@
+package expertostech.tutorial.rest.api.repository;
+
+
+import expertostech.tutorial.rest.api.controller.model.UsuarioModel;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UsuarioRepository extends CrudRepository<UsuarioModel, Integer>{
+
+
+}
